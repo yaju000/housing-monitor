d7d871d6ecfd3b59f7cdd07521945d608f84b5c6
zoewang <zoewang@micadat.com>
2026-04-01 11:45:07 +0800

    fix: use compareList as useEffect dep to avoid stale data on swap

--- frontend/src/pages/Compare.jsx
+++ frontend/src/pages/Compare.jsx
@@ -24,7 +24,7 @@ export default function Compare() {
       })
       .then(entries => setTransactionsMap(Object.fromEntries(entries)))
       .finally(() => setLoading(false))
-  }, [compareList.length])
+  }, [compareList])
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-8">
